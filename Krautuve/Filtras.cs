3500444873cede81e07eccdc08dfbeed24412c27
BernardasTicka <126004960+BernardasTicka@users.noreply.github.com>
2023-02-27 20:00:49 +0200

    Papildymas

--- Krautuve/Filtras.cs
+++ Krautuve/Filtras.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Krautuve;
+using System;
 
 namespace Krautuve
 {
-    internal class Filtras
+    public class Filtras
     {
         public static void BubbleSort(int[] arr)
         {
             int n = arr.Length;
             bool swapped;
+
             do
             {
                 swapped = false;
-                for (int i = 1; i < n; i++)
+
+                for (int i = 0; i < n - 1; i++)
                 {
-                    if (arr[i - 1] > arr[i])
+                    if (arr[i] > arr[i + 1])
                     {
-                        int temp = arr[i - 1];
-                        arr[i - 1] = arr[i];
-                        arr[i] = temp;
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+
                         swapped = true;
                     }
                 }
+
                 n--;
             } while (swapped);
         }
